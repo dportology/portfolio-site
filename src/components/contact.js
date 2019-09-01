b86f309ce201ca7e11918ab6e22dbda24d87cc77
balfur <gandalfsonofgloin@gmail.com>
2019-09-01 14:15:12 -0400

    added skype ID, removed copy pasta

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,7 +13,6 @@ class Contact extends Component {
                             alt="avatar"
                             style={{ height: '250px' }}
                         />
-                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target</p>
                     </Cell>
 
                     <Cell col={6}>
@@ -32,7 +31,7 @@ class Contact extends Component {
                                 <ListItem>
                                     <ListItemContent style={{ fontSize: '25px', fontFamily: "Anton", whiteSpace: "nowrap" }}>
                                         <i className="fa fa-skype" aria-hidden="true" />
-                                        Skype ID here
+                                        live:alex.davenport2194
                                     </ListItemContent>
                                 </ListItem>
 
